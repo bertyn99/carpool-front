87d9b51cf0b248ab9447fb85f648d3a0aba56832
Lucas <lucasperez702@gmail.com>
2023-04-06 14:48:00 +0200

    form get values

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,24 +2,23 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Input from '../../components/inputs/Input'
 import Button from '../../components/buttons/Button'
-import { SubmitHandler } from 'react-hook-form';
 
 const SignIn = () => {
-    const inputRef = useRef(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
 
-    useEffect(
-        () => {
-            console.log("input ref : " + inputRef)
-        }, [inputRef]
-    );
+    const handleSubmitEvent = (event) => {
+        event.preventDefault();
+        console.log("email value : " + emailRef.current?.value)
+        console.log("password value : " + passwordRef.current?.value)
+    }
 
     return (
-
         <>
             <div className='w-full py-52'>
-                <form method="post" className='flex flex-col justify-center items-center'>
-                    <Input label="Email" type="email" icon="Email" required ref={inputRef} />
-                    <Input label="Password" type="password" icon="Password" required ref={inputRef} />
+                <form method="post" onSubmit={handleSubmitEvent} className='flex flex-col justify-center items-center'>
+                    <Input label="Email" type="email" icon="Email" required ref={emailRef} />
+                    <Input label="Password" type="password" icon="Password" required ref={passwordRef} />
                     <Button label="Se connecter" type="submit" style="classic" />
                 </form>
             </div>
--- components/inputs/Input.tsx
+++ components/inputs/Input.tsx
@@ -1,18 +1,18 @@
 'use client';
 
-import React, { FC, InputHTMLAttributes, SetStateAction, useState, forwardRef } from 'react';
+import React, { FC, InputHTMLAttributes, SetStateAction, useState, forwardRef, useEffect } from 'react';
 import { FaRegEnvelope, FaEye } from 'react-icons/fa';
 
 interface props extends InputHTMLAttributes<HTMLInputElement> { label: string; type: string; icon: string; required: undefined | boolean; }
 
 export type Ref = HTMLInputElement;
 
-const Input: React.FC<props> = forwardRef<Ref, props>(({ label, type, icon, value, onChange }, ref) => {
+const Input = forwardRef<Ref, props>(({ label, type, icon, value, onChange }, ref) => {
 
     const Icons = ({ name }: { name: string }) => {
         if (name === "") {
             return (
-                <FaRegEnvelope fill-opacity="0%" />
+                <FaRegEnvelope fillOpacity="0%" />
             );
         } else if (name == "Email") {
             return (
@@ -27,7 +27,7 @@ const Input: React.FC<props> = forwardRef<Ref, props>(({ label, type, icon, valu
         return null
     }
 
-    let pattern = [{
+    let patternArray = [{
         'text': '[a-z0-9]{1,15}',
         'email': '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$',
     }]
@@ -39,7 +39,7 @@ const Input: React.FC<props> = forwardRef<Ref, props>(({ label, type, icon, valu
                 <input type={type} value={value} onChange={onChange} ref={ref}
                     className="bg-brown/25 rounded-l-lg py-1 pl-3 placeholder-dark-green/25 text-dark-green font-medium focus:ring-0 font-helvetica"
                     placeholder={type}
-                    pattern={type = "email" ? pattern[0].email : pattern[0].text}
+                    pattern={type === "email" ? patternArray[0].email : patternArray[0].text}
                     required
                 />
                 <div className='bg-brown/25 rounded-r-lg flex justify-center items-center pr-3 py-1 pl-3'>
