58128b23f1033a1b97e30edbc5ccf84af2d5a5a6
BigBOii <d.barlow@laposte.net>
2023-04-06 09:47:07 +0200

    fixed color

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       </button>
       <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
+        <ul className="flex flex-col p-4  rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium ">
             <Link className="block py-2 pl-3 pr-4 text-base" href="/" >Home</Link>
             <Link className="block py-2 pl-3 pr-4 text-base" href="/about">About</Link>
             <Link className="block py-2 pl-3 pr-4 text-base" href="/signin">SignIn</Link>
