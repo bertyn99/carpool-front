2d0f5c47852d9d6291af6c83eb1bfebe8b5c29a3
BigBOii <d.barlow@laposte.net>
2023-04-07 14:51:06 +0200

    added profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,52 +1,57 @@
+'use client' 
+import Button from '@/components/buttons/Button';
 import Modifyprofile from '@/components/modifyProfile/Modifyprofile';
-import React from 'react';
-import Button from '../../components/buttons/Button'
+import React, { useState } from 'react';
 
 const Profile = () => {
+    const [showModify, setShowModify] = useState(false);
+    console.log(showModify);
     const onClickModify = () => {
-        console.log("modify");
+        console.log("modify profile");
+        setShowModify(true);
     }
+    
     return (
-        <div className="w-full lg:w-4/12 px-4 mx-auto">
+        <div className="w-full  px-4 mx-auto">
             <div className="relative flex flex-col bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
                 <div className="px-6">
-                    <div className="flex flex-wrap justify-center">
-                        <div className="w-full px-4 flex justify-center">
-                            <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full ">
-                                <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"></path></svg>
-                            </div>
-                        </div>
-
+                <div className="flex flex-wrap justify-center">
+                    <div className="w-full px-4 flex justify-center">
+                    <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full ">
+                        <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"></path></svg>
                     </div>
-                    <div className="text-center">
-                        <h3 className="text-xl font-semibold leading-normal text-blueGray-700 ">
-                            Douglas Barlow
-                        </h3>
-                        <div className="font-bold">
-                            <i className=""></i>
-                            France - nantes
-                        </div>
                     </div>
-                    <div className="w-full text-center flex justify-center ">
-                        <div className="text-center">
-                            <span className="text-sm text-blueGray-400">Trips</span>
-                            <span className="text-xl font-bold block uppercase ">
-                                2
-                            </span>
-                            <span className="text-sm text-blueGray-400">Cars</span>
-                            <span className="text-xl font-bold block uppercase ">
-                                2
-                            </span>
-                        </div>
-                    </div>
-                    <div className="py-10 border-t border-blueGray-200 text-center">
-                        <div className="flex flex-wrap justify-center">
-                            <Button label="Se connecter" type="submit" style="classic" />
-                        </div>
+                
+                </div>
+                <div className="text-center">
+                    <h3 className="text-xl font-semibold leading-normal text-blueGray-700 ">
+                    Douglas Barlow
+                    </h3>
+                    <div className="font-bold">
+                    <i className=""></i>
+                    France - nantes
                     </div>
                 </div>
+                <div className="w-full text-center flex justify-center ">
+                        <div className="text-center">
+                        <span className="text-sm text-blueGray-400">Trips</span>
+                        <span className="text-xl font-bold block uppercase ">
+                            2
+                        </span>     
+                        <span className="text-sm text-blueGray-400">Cars</span>
+                        <span className="text-xl font-bold block uppercase ">
+                            2
+                        </span>                         
+                        </div>
+                    </div>
+                <div className="py-10 border-t border-blueGray-200 text-center">
+                     {showModify?<Modifyprofile/>:
+                     <div className="flex flex-wrap justify-center">
+                        <Button onClick={onClickModify} label="Modify Profile" type="submit" style="classic" />
+                    </div>}
+                </div>
+                </div>
             </div>
-            <Modifyprofile />
         </div>
     );
 };
--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -1,12 +1,12 @@
 import React, { FC, InputHTMLAttributes } from 'react';
 
-interface props extends InputHTMLAttributes<HTMLInputElement> { label: string; type?: 'submit' | 'reset' | 'button' | undefined; style: 'outlined' | 'classic' | undefined; }
+interface props extends InputHTMLAttributes<HTMLInputElement> { label: string; type?: 'submit' | 'reset' | 'button' | undefined; style: 'outlined' | 'classic' | undefined; onClick?: () => void; }
 
-const Button: React.FC<props> = ({ label, type, style }) => {
+const Button: React.FC<props> = ({ label, type, style, onClick }) => {
 
     return (
         <>
-            <button className={style == "classic"
+            <button onClick={onClick} className={style == "classic"
                 ? "text-cream bg-dark-green text-xl rounded-lg px-6 py-3 mt-4 hover:bg-light-green transition ease-in-out delay-50"
                 : "text-dark-green bg-cream border-2 border-dark-green rounded-lg px-4 py-2 hover:border-light-green hover:text-light-green transition ease-in-out delay-50"}
                 type={type}>{label}</button>
