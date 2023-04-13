ec82512d18e9388dd938443a4e00387307f26993
valentin-49 <valbouet@outlook.fr>
2023-04-13 08:26:35 +0200

    list of trips

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -38,6 +38,17 @@ const Search = () => {
             StartAddress: "Cholet",
             EndAddress: "Nantes"
         },
+        {
+          url: "https://picsum.photos/200/300",
+          urlalt: "car",
+          NameFirst: "Droguer",
+          NameLast: "Douglas",
+          StartDate: "11h00",
+          EndDate: "15h30",
+          Price: "25",
+          StartAddress: "Cholet",
+          EndAddress: "Nantes"
+      },
       ];
       return (
         <>
@@ -48,7 +59,7 @@ const Search = () => {
             <div className='mt-8 w-full flex flex-col justify-center items-center'>
 
             </div>
-            <div className='w-full flex flex-col justify-center items-center'>
+            <div className='w-full flex flex-col justify-center items-center mb-36'>
                 <TripsCardList cardInfoList={cardInfoList}/>  
             </div>
           </div>
