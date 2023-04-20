95fc62b722abd16f3390ccad5ef48c58f45ba042
Lucas <lucasperez702@gmail.com>
2023-04-20 08:43:28 +0200

    fix leaflet errors

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,7 +1,5 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
-import "leaflet-defaulticon-compatibility";
 
 const Map = () => {
     const position = [51.505, -0.09]
